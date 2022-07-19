515a22a5983c5ba34961de4642544b508a182248
Vignesh <Vignesh@DESKTOP-10QAI0L>
2022-07-19 11:47:17 +0530

    Project commit

---
+++ SampleMavenProject/src/test/java/org/maven/MavenPractice.java
@@ -0,0 +1,22 @@
+package org.maven;
+
+import org.testng.annotations.Test;
+
+public class MavenPractice {
+	@Test
+	public void Sample1()
+	{
+		System.out.println("MavenPratice1");
+	}
+	@Test
+	public void Sample2()
+	{
+		System.out.println("MavenPratice2");
+	}
+	@Test
+	public void Sample3()
+	{
+		System.out.println("MavenPratice3");
+	}
+
+}
